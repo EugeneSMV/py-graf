3046b6519c478f9cc35f94c625ce5356bdffb721
EugeneSMV <65439191+EugeneSMV@users.noreply.github.com>
2022-01-29 15:28:57 +0300

    Delete 01csv.py

--- 01csv.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('/home/smv/severovo_sv.csv', 'r', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
-
